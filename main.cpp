94082e78b3bc1b1cca9efdcc54e3f817f199606f
Hoang Nguyen <nguyeho2@oregonstate.edu>
2019-07-09 23:11:34 -0700

    Added for loop

--- main.cpp
+++ main.cpp
@@ -5,12 +5,18 @@ using namespace std;
 
 int main()
 {
-    string str;
+    string s1, s2;
     ifstream fin("main.cpp");
-    ofstream fout("text/test2.txt");
+    ofstream fout;
 
-    while (getline(fin, str)) {
-        fout << str + "\n";
+    while (fin >> s1) {
+        s2 += s1;
+    }
+    for (int i = 0; i < 100; i++) {
+        s1 = "text/test" + to_string(i) + ".txt";
+        fout.open(s1);
+        fout << s2;
+        fout.close();
     }
     return 0;
 }
