416b01df5e9181c5db544a704835b526293f38b2
Hoang Nguyen <nguyeho2@oregonstate.edu>
2019-07-15 22:51:52 -0700

    Added macros

--- main.cpp
+++ main.cpp
@@ -2,62 +2,60 @@
 #include <fstream> 
 #include <string> 
 #include <thread>
+#define l(x, n) for (int i = x; i < n; i++)
+#define p f << s[i] << endl;
+#define in(x) s[12].insert(x,1,q);
 using namespace std;
 
 void outF(string n)
 {
-    char q = 34;
-    char c = 44;
-    string s []= {"#include <iostream>",
+    char q = 34, c = 44;
+    string s []= {"#include <iostream>", 
                   "#include <fstream>",
                   "#include <string>",
                   "#include <thread>",
                   "using namespace std;",
+                  "#define l(x, n) for (int i = x; i < n; i++)",
+                  "#define p f << s[i] << endl;",
+                  "#define in(x) s[12].insert(x,1,q);",
                   "void outF(string n){",
-                  "char q = 34; char c = 44;",
+                  "char q = 34, c = 44;",
                   "string s []={",
                   "};",
                   "ofstream f(text/test + n);",
-                  "for (int i = 0; i < 8; i++) { f << s[i] << endl; }",
+                  "l(0, 11) { p }",
                   "for (string a : s) { f << q << a << q << c << endl; }",
-                  "s[9].insert(11, 1, q); s[9].insert(21, 1, q);",
-                  "for (int i = 8; i < 23; i++) { f << s[i] << endl; } }",
-                  "void loop(char j){ for (int i = 0; i < 187500; i++) { outF(j + to_string(i)); } }",
+                  "in(11) in(21)",
+                  "l(11, 23) { p } }",
+                  "void lo(char j){ l(0, 187500) { outF(j + to_string(i)); } }",
                   "int main(){",
-                  "loop(100);",
-                  "thread t1(loop, 97); thread t2(loop, 98); thread t3(loop, 99);",
+                  "lo(100);",
+                  "thread t1(lo, 97); thread t2(lo, 98); thread t3(lo, 99);",
                   "t1.join(); t2.join(); t3.join();",
                   "return 0;}"};
                
     ofstream f("text/test" + n);
-
-    for (int i = 0; i < 8; i++) {
-        f << s[i] << endl;
-    }
+    l(0, 11) { p }
     for (string a : s) {
         f << q << a << q << c << endl;
     }
-    s[9].insert(11, 1, q);
-    s[9].insert(21, 1, q);
-    for (int i = 8; i < 20; i++) {
-        f << s[i] << endl;
-    }
+    in(11) in(21)
+    l(11, 23) { p }
 }
 
-void loop(char j)
+void lo(char j)
 {
-    for (int i = 0; i < 187500; i++) {
+    l(0, 187500) {
         outF(j + to_string(i));
     }
 }
 
-
 int main() 
 {
-    loop(100);
-    thread t1(loop, 97);
-    thread t2(loop, 98);
-    thread t3(loop, 99);
+    lo(100);
+    thread t1(lo, 97);
+    thread t2(lo, 98);
+    thread t3(lo, 99);
     t1.join();
     t2.join();
     t3.join();
