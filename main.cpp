38487f50a8813ce3b222244417ae5730f7d9c2aa
Hoang Nguyen <nguyeho2@oregonstate.edu>
2019-07-19 23:31:53 -0700

    Further optimization

--- main.cpp
+++ main.cpp
@@ -1,63 +1,24 @@
-#include <iostream> 
+char s [12][200]= {
+"char s [12][200]={",
+"};",
+"#include <fstream>",
+"#include <thread>",
+"#define l(x, n, d) for (int i = x; i < n; i++) {d;}",
+"#define p(x) f << x << end;",
+"#define tc(n, e) thread n (lo, e);",
+"#define w(n) n.join();",
+"using namespace std;",
+"void outF(string n){char q = 34, c = 44; ofstream f(n); p(s[0]) l(0, 12, p(q << s[i] << q << c)) l(1, 12, p(s[i]))}",
+"void lo(char j){l(0, 187500, outF(j + to_string(i)))}",
+"int main(){lo(104);tc(t1, 97) tc(t2, 98) tc(t3, 99) tc(t4, 100) tc(t5, 101) tc(t6, 102) tc(t7, 103) w(t1) w(t2) w(t3) w(t4) w(t5) w(t6) w(t7)}"
+};
 #include <fstream> 
-#include <string> 
 #include <thread>
-#define l(x, n) for (int i = x; i < n; i++)
-#define p f << s[i] << endl;
-#define in(x) s[12].insert(x,1,q);
+#define l(x, n, d) for (int i = x; i < n; i++) {d;}
+#define p(x) f << x << endl;
+#define tc(n, e) thread n (lo, e);
+#define w(n) n.join();
 using namespace std;
-
-void outF(string n)
-{
-    char q = 34, c = 44;
-    string s []= {"#include <iostream>", 
-                  "#include <fstream>",
-                  "#include <string>",
-                  "#include <thread>",
-                  "using namespace std;",
-                  "#define l(x, n) for (int i = x; i < n; i++)",
-                  "#define p f << s[i] << endl;",
-                  "#define in(x) s[12].insert(x,1,q);",
-                  "void outF(string n){",
-                  "char q = 34, c = 44;",
-                  "string s []={",
-                  "};",
-                  "ofstream f(text/test + n);",
-                  "l(0, 11) { p }",
-                  "for (string a : s) { f << q << a << q << c << endl; }",
-                  "in(11) in(21)",
-                  "l(11, 23) { p } }",
-                  "void lo(char j){ l(0, 187500) { outF(j + to_string(i)); } }",
-                  "int main(){",
-                  "lo(100);",
-                  "thread t1(lo, 97); thread t2(lo, 98); thread t3(lo, 99);",
-                  "t1.join(); t2.join(); t3.join();",
-                  "return 0;}"};
-               
-    ofstream f("text/test" + n);
-    l(0, 11) { p }
-    for (string a : s) {
-        f << q << a << q << c << endl;
-    }
-    in(11) in(21)
-    l(11, 23) { p }
-}
-
-void lo(char j)
-{
-    l(0, 187500) {
-        outF(j + to_string(i));
-    }
-}
-
-int main() 
-{
-    lo(100);
-    thread t1(lo, 97);
-    thread t2(lo, 98);
-    thread t3(lo, 99);
-    t1.join();
-    t2.join();
-    t3.join();
-    return 0;
-}
+void outF(string n){char q = 34, c = 44; ofstream f(n); p(s[0]) l(0, 12, p(q << s[i] << q << c)) l(1, 12, p(s[i]))}
+void lo(char j){l(0, 125, outF(j + to_string(i)))}
+int main(){lo(104);tc(t1, 97) tc(t2, 98) tc(t3, 99) tc(t4, 100) tc(t5, 101) tc(t6, 102) tc(t7, 103) w(t1) w(t2) w(t3) w(t4) w(t5) w(t6) w(t7)}
