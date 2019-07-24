cb3c526682256f443cf4d4d976284be42ce03c77
Hoang Nguyen <nguyeho2@oregonstate.edu>
2019-07-23 23:09:48 -0700

    Changed to use raw string literals

--- main.cpp
+++ main.cpp
@@ -1,24 +1,23 @@
-char s [12][200]= {
-"char s [12][200]={",
+char s [3][550]= {
+"char s [3][550]={",
 "};",
-"#include <fstream>",
-"#include <thread>",
-"#define l(x, n, d) for (int i = x; i < n; i++) {d;}",
-"#define p(x) f << x << end;",
-"#define tc(n, e) thread n (lo, e);",
-"#define w(n) n.join();",
-"using namespace std;",
-"void outF(string n){char q = 34, c = 44; ofstream f(n); p(s[0]) l(0, 12, p(q << s[i] << q << c)) l(1, 12, p(s[i]))}",
-"void lo(char j){l(0, 187500, outF(j + to_string(i)))}",
-"int main(){lo(104);tc(t1, 97) tc(t2, 98) tc(t3, 99) tc(t4, 100) tc(t5, 101) tc(t6, 102) tc(t7, 103) w(t1) w(t2) w(t3) w(t4) w(t5) w(t6) w(t7)}"
-};
-#include <fstream> 
+R"(#include <fstream>
 #include <thread>
-#define l(x, n, d) for (int i = x; i < n; i++) {d;}
-#define p(x) f << x << endl;
-#define tc(n, e) thread n (lo, e);
+#define l(x,n,d) for(int i=x;i<n;i++){d;}
+#define p(x) f<<x<<endl;
+#define tc(n,e) thread n (lo,e);
 #define w(n) n.join();
 using namespace std;
-void outF(string n){char q = 34, c = 44; ofstream f(n); p(s[0]) l(0, 12, p(q << s[i] << q << c)) l(1, 12, p(s[i]))}
-void lo(char j){l(0, 125, outF(j + to_string(i)))}
-int main(){lo(104);tc(t1, 97) tc(t2, 98) tc(t3, 99) tc(t4, 100) tc(t5, 101) tc(t6, 102) tc(t7, 103) w(t1) w(t2) w(t3) w(t4) w(t5) w(t6) w(t7)}
+void ou(string n){char q=34,c=44,a=40,r=82;ofstream f(n);p(s[0])l(0,2,p(q<<s[i]<<q<<c))p(r<<q<<a<<s[2]<<++a<<q<<s[1])p(s[2])}
+void lo(char j){l(0,93750,ou(j+to_string(i)))}
+int main(){lo(55);tc(t1,48)tc(t2,49)tc(t3,50)tc(t4,51)tc(t5,52)tc(t6,53)tc(t7,54)w(t1)w(t2)w(t3)w(t4)w(t5)w(t6)w(t7)})"};
+#include <fstream> 
+#include <thread>
+#define l(x,n,d) for(int i=x;i<n;i++){d;}
+#define p(x) f<<x<<endl;
+#define tc(n,e) thread n (lo,e);
+#define w(n) n.join();
+using namespace std;
+void ou(string n){char q=34,c=44,a=40,r=82;ofstream f(n);p(s[0])l(0,2,p(q<<s[i]<<q<<c))p(r<<q<<a<<s[2]<<++a<<q<<s[1])p(s[2])}
+void lo(char j){l(0,93750,ou(j+to_string(i)))}
+int main(){lo(55);tc(t1,48)tc(t2,49)tc(t3,50)tc(t4,51)tc(t5,52)tc(t6,53)tc(t7,54)w(t1)w(t2)w(t3)w(t4)w(t5)w(t6)w(t7)}
