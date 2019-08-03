f73f503155ff0c173dec6a7f242eba9f759e5343
Hoang Nguyen <nguyeho2@oregonstate.edu>
2019-08-03 12:19:34 -0700

    Reduced character count and increased run time

--- main.cpp
+++ main.cpp
@@ -1,23 +1,13 @@
-char s [3][550]= {
-"char s [3][550]={",
-"};",
-R"(#include <fstream>
-#include <thread>
-#define l(x,n,d) for(int i=x;i<n;i++){d;}
-#define p(x) f<<x<<endl;
-#define tc(n,e) thread n (lo,e);
+char s[]=R"(char s[]=R"(%s%c";
+#include<cstdio>
+#include<thread>
+#define tc(n,e) std::thread n (p,e);
 #define w(n) n.join();
-using namespace std;
-void ou(string n){char q=34,c=44,a=40,r=82;ofstream f(n);p(s[0])l(0,2,p(q<<s[i]<<q<<c))p(r<<q<<a<<s[2]<<++a<<q<<s[1])p(s[2])}
-void lo(char j){l(0,93750,ou(j+to_string(i)))}
-int main(){lo(55);tc(t1,48)tc(t2,49)tc(t3,50)tc(t4,51)tc(t5,52)tc(t6,53)tc(t7,54)w(t1)w(t2)w(t3)w(t4)w(t5)w(t6)w(t7)})"};
-#include <fstream> 
-#include <thread>
-#define l(x,n,d) for(int i=x;i<n;i++){d;}
-#define p(x) f<<x<<endl;
-#define tc(n,e) thread n (lo,e);
+void p(int a) {char n[8];for(int i=0;i<93750;i++){itoa(a*93750+i,n,10);FILE *f=fopen(n,"w");fprintf(f,z);fclose(f);}}
+int main(){p(7);tc(t1,0)tc(t2,1)tc(t3,2)tc(t4,3)tc(t5,4)tc(t6,5)tc(t7,6)w(t1)w(t2)w(t3)w(t4)w(t5)w(t6)w(t7)})";
+#include<cstdio>
+#include<thread>
+#define tc(n,e) std::thread n (p,e);
 #define w(n) n.join();
-using namespace std;
-void ou(string n){char q=34,c=44,a=40,r=82;ofstream f(n);p(s[0])l(0,2,p(q<<s[i]<<q<<c))p(r<<q<<a<<s[2]<<++a<<q<<s[1])p(s[2])}
-void lo(char j){l(0,93750,ou(j+to_string(i)))}
-int main(){lo(55);tc(t1,48)tc(t2,49)tc(t3,50)tc(t4,51)tc(t5,52)tc(t6,53)tc(t7,54)w(t1)w(t2)w(t3)w(t4)w(t5)w(t6)w(t7)}
+void p(int a) {char n[8];for(int i=0;i<93750;i++){itoa(a*93750+i,n,10);FILE *f=fopen(n,"w");fprintf(f,s,s,41);fclose(f);}}
+int main(){p(7);tc(t1,0)tc(t2,1)tc(t3,2)tc(t4,3)tc(t5,4)tc(t6,5)tc(t7,6)w(t1)w(t2)w(t3)w(t4)w(t5)w(t6)w(t7)}
