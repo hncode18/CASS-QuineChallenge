1578e3beebae43f49c408c011a28397838baa1aa
Hoang Nguyen <nguyeho2@oregonstate.edu>
2019-08-03 12:48:18 -0700

    Further reduced character count and runtime

--- main.cpp
+++ main.cpp
@@ -1,13 +1,11 @@
 char s[]=R"(char s[]=R"(%s%c";
 #include<cstdio>
-#include<thread>
-#define tc(n,e) std::thread n (p,e);
-#define w(n) n.join();
-void p(int a) {char n[8];for(int i=0;i<93750;i++){itoa(a*93750+i,n,10);FILE *f=fopen(n,"w");fprintf(f,z);fclose(f);}}
-int main(){p(7);tc(t1,0)tc(t2,1)tc(t3,2)tc(t4,3)tc(t5,4)tc(t6,5)tc(t7,6)w(t1)w(t2)w(t3)w(t4)w(t5)w(t6)w(t7)})";
+void p(){char n[8];for(int i=0;i<750000;i++){sprintf(n,"%d",i);FILE *f=fopen(n,"w");fprintf(f,s,s,41);fclose(f);}}
+int main(){
+#pragma omp parallel num_threads(8)
+{p();}})";
 #include<cstdio>
-#include<thread>
-#define tc(n,e) std::thread n (p,e);
-#define w(n) n.join();
-void p(int a) {char n[8];for(int i=0;i<93750;i++){itoa(a*93750+i,n,10);FILE *f=fopen(n,"w");fprintf(f,s,s,41);fclose(f);}}
-int main(){p(7);tc(t1,0)tc(t2,1)tc(t3,2)tc(t4,3)tc(t5,4)tc(t6,5)tc(t7,6)w(t1)w(t2)w(t3)w(t4)w(t5)w(t6)w(t7)}
+void p(){char n[8];for(int i=0;i<750000;i++){sprintf(n,"%d",i);FILE *f=fopen(n,"w");fprintf(f,s,s,41);fclose(f);}}
+int main(){
+#pragma omp parallel num_threads(8)
+{p();}}
